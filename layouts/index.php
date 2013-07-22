d92185a035f081cb6cf0475fb17d3e435ac2889b
Michael Babker <mbabker@flbab.com>
2013-07-22 07:40:57 -0500

    Google Analytics

--- layouts/index.php
+++ layouts/index.php
@@ -396,5 +396,20 @@
 		<jdoc:include type="modules" name="analytics" />
 		<?php endif ?>
 
+		<script>
+			(function(i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] || function() {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+				a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+			ga('create', 'UA-42602973-1', 'babdev.com');
+			ga('send', 'pageview');
+		</script>
 	</body>
 </html>
